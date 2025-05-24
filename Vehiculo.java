5e8a34eb9939ddae22c484d080ffa845948716c9
JuanCasZa <154453381+JuanCasZa@users.noreply.github.com>
2025-05-23 22:25:33 -0500

    Update Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -12,6 +12,7 @@ public class Vehiculo {
   protected String marca;
   protected String placa;
   protected String numeroEstacionamiento;
+  private Factura factura;
   public static Scanner sc = new Scanner(System.in);
 
   // Constructores
@@ -57,6 +58,10 @@ public class Vehiculo {
     this.numeroEstacionamiento = numeroEstacionamiento;
   }
 
+  public Factura getFactura(){
+    return factura;
+  }
+
   // vale: Método para validar placa: 3 letras seguidas de 3 números
   // Para carro: 3 letras + 3 números, solo mayúsculas (ej: ABC123)
   /*
@@ -82,168 +87,142 @@ public class Vehiculo {
    * carros de las motos, como para que no parezca que tienen exactamente los
    * mismos atributos, porque entonces no tendria mucho sentido el cambio que hice
    */
-  public static void agregarVehiculo(int tipoVehiculo) {
-    int numeroEstacionamientoParaRegistro = 0;
-
-    switch (tipoVehiculo) {
-      case 1:
-        // Para carro
-        Carro.agregarCarro();
-        break;
-      case 2:
-        Moto.agregarMoto();
-        // Para moto
-        break;
-    }
-    System.out.println("Se ha registrado con exito !!");
-  }
-
-  /*
-   * Acá nuevamente, en quitar vehiculo hice lo mismo, poniendo el proceso en cada
-   * clase espesifica. Recalco que, tanto carro, como moto, las clases, tienen
-   * vectores
-   * o matrices abjetuales de diferentes dimensiones. Y cada una es del mismo tipo
-   * de la clase.
-   * 
-   * Como los vectores objetuales son estaticos, fijos, es decir siempre
-   * utilizaremos el mismo, por eso los metodos no reciben como parametro los
-   * vectores,
-   * y por eso mismo tampoco me devuelven algo
-   */
-
-  public static void quitarVehiculo(int tipoVehiculo) {
-
-    System.out.print("\nIngrese la placa del vehiculo: ");
-    String placa = sc.next();
-
-    switch (tipoVehiculo) {
-      case 1:
-        // Para carro
-        Carro.quitarVehiculoCarro(placa);
-        break;
-      case 2:
-        // Para moto
-        Moto.quitarVehiculoMoto(placa);
-        break;
-    }
-  }
-
-  public static void modificarVehiculo(int tipoVehiculo) {
-
-    System.out.print("\nIngrese el numero de estacionamiento: ");
-    int puesto = sc.nextInt();
-
-    switch (tipoVehiculo) {
-      case 1:
-        // Para carro
-        Carro.modificarInformacionCarro(puesto);
-        break;
-      case 2:
-        // Para moto
-        Moto.modificarInformacionMoto(puesto);
-        break;
-    }
-    System.out.println("El vehiculo se ha modificado con exito !!\n");
-  }
-
   public static void mostrarVehiculos() {
-    System.out.print("\n\tVehiculos");
-    System.out.print("\nCarros\n");
-    for (int i = 0; i < Carro.espaciosPisoCarro.length; i++) {
-      if (Carro.espaciosPisoCarro[i] != false) {
-        System.out.print("Marca: " + Carro.espaciosCarro[i].getMarca().toUpperCase() + "\n");
-        System.out.print("Color: " + Carro.espaciosCarro[i].getColor().toUpperCase() + "\n");
-        System.out.print("Número puertas: " + Carro.espaciosCarro[i].getNumeroDePuertas() + "\n");
-        System.out.print("Tipo: " + Carro.espaciosCarro[i].getTipoDeVehiculo().toUpperCase() + "\n");
-        System.out.print("Placa: " + Carro.espaciosCarro[i].getPlaca().toUpperCase() + "\n");
-        System.out.print(
-            "Estacionado en el puesto: " + Carro.espaciosCarro[i].getNumeroEstacionamiento().toUpperCase() + "\n");
-        System.out.println();
-      } else {
-        continue;
-      }
-    }
-    System.out.print("\nMotos\n");
-    for (int i = 0; i < Moto.espaciosPisoMoto.length; i++) {
-      if (Moto.espaciosPisoMoto[i] != false) {
-        System.out.print("Marca: " + Moto.espaciosMoto[i].getMarca().toUpperCase() + "\n");
-        System.out.print("Color: " + Moto.espaciosMoto[i].getColor().toUpperCase() + "\n");
-        System.out.print("Cilindraje: " + Moto.espaciosMoto[i].getCilindraje() + "\n");
-        System.out.print("Placa: " + Moto.espaciosMoto[i].getPlaca().toUpperCase() + "\n");
-        System.out
-            .print("Estacionado en el puesto: " + Moto.espaciosMoto[i].getNumeroEstacionamiento().toUpperCase() + "\n");
-        System.out.println();
-      } else {
-        continue;
-      }
-    }
-  }
+		System.out.print("\n\tVehiculos");
+		System.out.print("\nCarros\n");
+		for (int i = 0; i < Carro.espaciosPisoCarro.length; i++) {
+			if(Carro.espaciosPisoCarro[i]!=false){
+					System.out.print("Marca: "+Carro.espaciosCarro[i].getMarca()+"\n");
+					System.out.print("Color: "+Carro.espaciosCarro[i].getColor()+"\n");
+					System.out.print("Número puertas: "+Carro.espaciosCarro[i].getNumeroDePuertas()+"\n");
+					System.out.print("Tipo: "+Carro.espaciosCarro[i].getTipoDeVehiculo()+"\n");
+					System.out.print("Placa: "+Carro.espaciosCarro[i].getPlaca()+"\n");
+					System.out.print("Estacionado en el puesto: "+Carro.espaciosCarro[i].getNumeroEstacionamiento()+"\n");
+					System.out.println();
+			}else {
+				continue;
+			}	
+		}
+		System.out.print("\nMotos\n");
+		for (int i = 0; i < Moto.espaciosPisoMoto.length; i++) {
+			if(Moto.espaciosPisoMoto[i]!=false){
+					System.out.print("Marca: "+Moto.espaciosMoto[i].getMarca()+"\n");
+					System.out.print("Color: "+Moto.espaciosMoto[i].getColor()+"\n");
+					System.out.print("Cilindraje: "+Moto.espaciosMoto[i].getCilindraje()+"cc"+"\n");
+					System.out.print("Placa: "+Moto.espaciosMoto[i].getPlaca()+"\n");
+					System.out.print("Estacionado en el puesto: "+Moto.espaciosMoto[i].getNumeroEstacionamiento()+"\n");
+					System.out.println();
+			}else {
+				continue;
+			}	
+		}
+	}
+	
+	public static void mostrarVehiculoPorPlaca() {
+		System.out.print("Ingrese placa: ");
+		String placa = sc.next();
+		
+		for (int i = 0; i < Carro.espaciosPisoCarro.length; i++) {
+			if(Carro.espaciosPisoCarro[i]!=false){
+				if(Carro.espaciosCarro[i].getPlaca().equals(placa)) {
+					System.out.print("Marca: "+Carro.espaciosCarro[i].getMarca()+"\n");
+					System.out.print("Color: "+Carro.espaciosCarro[i].getColor()+"\n");
+					System.out.print("Número puertas: "+Carro.espaciosCarro[i].getNumeroDePuertas()+"\n");
+					System.out.print("Tipo: "+Carro.espaciosCarro[i].getTipoDeVehiculo()+"\n");
+					System.out.print("Placa: "+Carro.espaciosCarro[i].getPlaca()+"\n");
+					System.out.print("Estacionado en el puesto: "+Carro.espaciosCarro[i].getNumeroEstacionamiento()+"\n");
+					System.out.println();
+				}
+			}else {
+				continue;
+			}	
+		}
+		for (int i = 0; i < Moto.espaciosPisoMoto.length; i++) {
+			if(Moto.espaciosPisoMoto[i]!=false){
+				if(Moto.espaciosMoto[i].getPlaca().equals(placa)) {
+					System.out.print("Marca: "+Moto.espaciosMoto[i].getMarca()+"\n");
+					System.out.print("Color: "+Moto.espaciosMoto[i].getColor()+"\n");
+					System.out.print("Cilindraje: "+Moto.espaciosMoto[i].getCilindraje()+"\n");
+					System.out.print("Placa: "+Moto.espaciosMoto[i].getPlaca()+"\n");
+					System.out.print("Estacionado en el puesto: "+Moto.espaciosMoto[i].getNumeroEstacionamiento()+"\n");
+					System.out.println();
+				}
+			}else {
+				continue;
+			}	
+		}
+	}
+	
+	public static String contabilizacionVehiculos(int opcion) {
 
-  public static String contabilizacionVehiculos(int opcion) {
+		int contadorEspaciosVacios =0, contadorEspaciosOcupados=0;
+		String respuesta = "";
+		
+		for (int i = 0; i < Carro.espaciosPisoCarro.length; i++) {
+			if (Carro.espaciosPisoCarro[i] != false) {
+				contadorEspaciosOcupados++;
+			} else {
+				contadorEspaciosVacios++;
+			}
+		}
+		
+		for (int i = 0; i < Moto.espaciosPisoMoto.length; i++) {
+			if (Moto.espaciosPisoMoto[i] != false) {
+				contadorEspaciosOcupados++;
+			} else {
+				contadorEspaciosVacios++;
+			}
+		}
+		
+		if (opcion == 1) {
+			respuesta = "La cantidad de espacios ocupados es de: "+contadorEspaciosOcupados;
+		}else {
+			respuesta = "\nLa cantidad de espacios vacios es de: "+contadorEspaciosVacios;
+		}
+		
+		return respuesta+"\n";
+	}
+	
+	public static void guardarVehiculosEnArchivo(int tipoVehiculo) {
+		
+		String direccionArchivoPlano = "";
+		
+		switch (tipoVehiculo) {
+		case 1:
+			//Carro
+			direccionArchivoPlano = "Carros.dat";
+			Carro.guardarVehiculoEnArchivo(direccionArchivoPlano);
+			break;
 
-    int contadorEspaciosVacios = 0, contadorEspaciosOcupados = 0;
-    String respuesta = "";
+		case 2:
+			//Moto
+			direccionArchivoPlano = "Motos.dat";
+			Moto.guardarVehiculoEnArchivo(direccionArchivoPlano);
+			break;
+		}
+	}
+	
+	public static void cargarInformacionVehiculos(int tipoVehiculo) {
+		
+		String direccionArchivoPlano = "";
+		
+		switch (tipoVehiculo) {
+		case 1:
+			//Carro
+			direccionArchivoPlano = "Carros.dat";
+			Carro.cargarInformacion(direccionArchivoPlano);
+			break;
 
-    for (int i = 0; i < Carro.espaciosPisoCarro.length; i++) {
-      if (Carro.espaciosPisoCarro[i] != false) {
-        contadorEspaciosOcupados++;
-      } else {
-        contadorEspaciosVacios++;
-      }
-    }
+		case 2:
+			//Moto
+			direccionArchivoPlano = "Motos.dat";
+			Moto.cargarInformacion(direccionArchivoPlano);
+			break;
+		}
+	}
 
-    for (int i = 0; i < Moto.espaciosPisoMoto.length; i++) {
-      if (Moto.espaciosPisoMoto[i] != false) {
-        contadorEspaciosOcupados++;
-      } else {
-        contadorEspaciosVacios++;
-      }
-    }
-
-    if (opcion == 1) {
-      respuesta = "La cantidad de espacios ocupados es de: " + contadorEspaciosOcupados;
-    } else {
-      respuesta = "\nLa cantidad de espacios vacios es de: " + contadorEspaciosVacios;
-    }
-
-    return respuesta + "\n";
-  }
-
-  public static void guardarVehiculosEnArchivo(int tipoVehiculo) {
-
-    String direccionArchivoPlano = "";
-
-    switch (tipoVehiculo) {
-      case 1:
-        // Carro
-        direccionArchivoPlano = "C://Users/USUARIO/Desktop/Vehiculos-Carros.txt/";
-        Carro.guardarVehiculoEnArchivo(direccionArchivoPlano);
-        break;
-
-      case 2:
-        // Moto
-        direccionArchivoPlano = "C:/Users/USUARIO/Desktop/Vehiculos-Motos.txt";
-        Moto.guardarVehiculoEnArchivo(direccionArchivoPlano);
-        break;
-    }
-  }
-
-  public static void cargarInformacionVehiculos(int tipoVehiculo) {
-
-    String direccionArchivoPlano = "";
-
-    switch (tipoVehiculo) {
-      case 1:
-        // Carro
-        direccionArchivoPlano = "C://Users/USUARIO/Desktop/Vehiculos-Carros.txt/";
-        Carro.cargarInformacion(direccionArchivoPlano);
-        break;
-
-      case 2:
-        // Moto
-        direccionArchivoPlano = "C:/Users/USUARIO/Desktop/Vehiculos-Motos.txt";
-        Moto.cargarInformacion(direccionArchivoPlano);
-        break;
-    }
-  }
+	@Override
+	public String toString() {
+		return color + "," + marca + "," + placa + "," + numeroEstacionamiento;
+	}
 }
