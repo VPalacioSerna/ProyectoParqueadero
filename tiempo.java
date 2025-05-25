2be0e3dc7c743dbe1dc1910315820716615613a3
mono1106 <yesidsoryesid@gmail.com>
2025-05-24 22:49:56 -0500

    Create tiempo.java

---
+++ tiempo.java
@@ -0,0 +1,31 @@
+import java.time.format.DateTimeFormatter;// se usa en los dos metodos para importar o traer fecha y hora
+import java.time.LocalDate;//metodo para slo tener la fecha
+import java.time.LocalTime;//metodo solo para tener la hora
+
+public class Tiempo {
+public Steing dia (){  
+        LocalDate fecha = LocalDate.now();
+        DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yy");
+        String dia = fecha.format(formatoFecha);
+        return (dia);
+    }
+    public int hora (){
+        LocalTime hora = LocalTime.now();      
+        DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+        // es la variable para guardar la hora como tal
+        String tiempo = hora.format(formatoHora);
+        // la hora es formato HH:mm:ss y para identificar cada parte se usa : como separlo,guardarlo en el vector y luego convertirlo a entero 
+        String [] partes = tiempo.split(":");
+        //("hora: "+partes[0]);
+        //("minuto: "+partes[1]);
+        //("segundos: "+partes[2]);
+        
+        int h1= Integer.parseInt(partes[0]);
+        int m1= Integer.parseInt(partes[1]);
+        int s1= Integer.parseInt(partes[2]);
+        
+        int Total=((h1*60)+m1+(s1/60));
+        return (Total);       
+    }
+
+}
